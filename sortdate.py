acda88e9edb1dc478ab8ec39c5ff0957d60f71e3
rishijavia <rishi.javia.3895@gmail.com>
2017-04-23 16:42:23 -0700

    added ANOVA for every hour

--- sortdate.py
+++ sortdate.py
@@ -4,6 +4,7 @@ from pprint import pprint
 from datetime import datetime
 from dateutil import tz
 import statistics as stat
+import math
 
 from_zone = tz.gettz('UTC')
 to_zone = tz.gettz('America/Los_Angeles')
@@ -42,9 +43,11 @@ for data in miller_data:
     if not len(miller_hourly_data[data["hour"]]) == 0:
         miller_hourly_data[data["hour"]]["download"].append(data["download"])
         miller_hourly_data[data["hour"]]["upload"].append(data["upload"])
+        miller_hourly_data[data["hour"]]["count"] += 1
     else:
         hour_dict = {}
         hour_dict["hour"] = data["hour"]
+        hour_dict["count"] = 1
         hour_dict["download"] = [data["download"]]
         hour_dict["upload"] = [data["upload"]]
         miller_hourly_data[data["hour"]] = hour_dict
@@ -60,5 +63,53 @@ for data in miller_hourly_data:
 hourly_mean_download = [data["mean_download"] for data in miller_hourly_data]
 hourly_mean_upload = [data["mean_upload"] for data in miller_hourly_data]
 
-pprint(hourly_mean_download)
-pprint(hourly_mean_upload)
+n_list = []
+mean_list_download = []
+s_list_download = []
+mean_list_upload = []
+s_list_upload = []
+for data in miller_hourly_data:
+    n_list.append(data["count"])
+    mean_list_download.append(data["mean_download"])
+    mean_list_upload.append(data["mean_upload"])
+    s_list_download.append(data["sample_sd_download"])
+    s_list_upload.append(data["sample_sd_upload"])
+
+
+# pprint(hourly_mean_download)
+# pprint(hourly_mean_upload)
+
+# Andrea's functions:
+
+def ss_between(n, mean_list, mean):
+    ss = 0
+    for i in range(len(n)):
+        ss += (n[i] * (mean_list[i] - mean)*(mean_list[i] - mean))
+    return ss
+def ss_within(n, s):
+    ss = 0
+    for i in range(len(n)):
+        ss += (n[i] - 1) * math.pow(s[i], 2)
+    return ss
+
+def weighted_avg(n, means):
+    avg = 0
+    for i in range(len(n)):
+        avg += (n[i]/sum(n)) * means[i]
+    return avg
+def anova(n_list, mean_list, s_list):
+    n_star = sum(n_list)
+    overall_avg = weighted_avg(n_list, mean_list)
+    df_between = len(n_list) - 1
+    df_within = n_star - len(n_list)
+    df_total = n_star - 1
+    ss_b = ss_between(n_list, mean_list, overall_avg)
+    ss_w = ss_within(n_list, s_list)
+    ms_between = ss_b / df_between
+    ms_within = ss_w / df_within
+    f = ms_between / ms_within
+    print "1 - pf(" + str(f) + "," + str(df_between) + "," + str(df_total) + ")"
+    return f
+
+anova(n_list, mean_list_download, s_list_upload)
+anova(n_list, mean_list_upload, s_list_upload)
